0aaa489a1d105b86f84110d2a3eecc0a2e8bc9e3
TassyakurPasya <55564127+TassyakurPasya@users.noreply.github.com>
2019-09-25 18:30:44 +0700

    Add files via upload

---
+++ Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teks;
+
+/**
+ *
+ * @author Lenovo
+ * Nama : Tassyakur Pasya
+ * Kelas : IF-01
+ * NIM : 10118009
+ * Matkul : PBO-01
+ * Deskripsi Program : Teks
+ */
+public class Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
